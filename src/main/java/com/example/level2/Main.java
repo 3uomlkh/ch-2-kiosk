665863e293ea8e2bbcb8bfab5cda14698515bfed
3uomlkh <3uomlkh@gmail.com>
2025-01-14 11:47:04 +0900

    refactor: Lv.2 - 메뉴 초기화 기능 메서드로 분리

--- src/main/java/com/example/level2/Main.java
+++ src/main/java/com/example/level2/Main.java
@@ -7,11 +7,7 @@ import java.util.Scanner;
 public class Main {
     static final List<MenuItem> menus = new ArrayList<>();
     public static void main(String[] args) {
-        menus.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menus.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        menus.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        menus.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
-
+        initializeMenu();
         Scanner sc = new Scanner(System.in);
         while (true) {
             showMenu();
@@ -36,6 +32,13 @@ public class Main {
             }
             System.out.println();
         }
+    }
+
+    private static void initializeMenu() {
+        menus.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menus.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menus.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menus.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
     }
 
     private static void showMenu() {
