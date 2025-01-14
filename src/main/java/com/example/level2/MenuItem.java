ca6679779a517bd72b8e620a7c6553f49fa34399
3uomlkh <3uomlkh@gmail.com>
2025-01-14 10:57:38 +0900

    feat: Lv.2 - MenuItem 클래스 구현

---
+++ src/main/java/com/example/level2/MenuItem.java
@@ -0,0 +1,13 @@
+package com.example.level2;
+
+public class MenuItem {
+    private String name;
+    private int price;
+    private String description;
+
+    public MenuItem(String name, int price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+}
