00d87b9b3dc40419fe00fbfc9b0c16fbb480314a
3uomlkh <3uomlkh@gmail.com>
2025-01-20 10:29:44 +0900

    feat: Lv.6 - 장바구니를 비우는 기능 구현

--- src/main/java/com/example/level6/Cart.java
+++ src/main/java/com/example/level6/Cart.java
@@ -34,6 +34,9 @@ public class Cart {
                     item.getDescription(),
                     items.get(item));
         }
+    }
 
+    public void clearCart() {
+        items.clear();
     }
 }
